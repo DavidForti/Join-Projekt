f70a1698d729d6be2e594fe2e1fbc543004d049c
Bernd Troßmann <114260977+ben1070XXL@users.noreply.github.com>
2022-12-26 10:57:13 +0100

    Added Basic Task To Backend

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 let users = [];
-// let users = [
-//     {
-//         "name": "Fritz Meier",
-//         "email": "f.meier@web.de",
-//         "password": "1234"
-//     },
-//     {
-//         "name": "Andreas Huber",
-//         "email": "a.huber@web.de",
-//         "password": "4321"
-//     }
-// ];
-
+let tasks = [];
 
 // Change BASE_SERVER_URL for smallest_backend_ever
 setURL('https://gruppe-411.developerakademie.net/smallest_backend_ever');
@@ -29,27 +17,72 @@ async function init() {
         msgbox.innerHTML = msg;
     }
 
-    // await deleteAllUsers();
+    // await deleteAll();
     // await saveUsersToBackend();
+    //await saveTasksToBackend();
     await downloadFromServer();
     users = JSON.parse(backend.getItem('users')) || [];
 
-    
+    // tasks = JSON.parse(backend.getItem('tasks')) || [];
+
 }
 
 
 async function saveUsersToBackend() {
+    users = [
+        {
+            "name": "Fritz Meier",
+            "email": "f.meier@web.de",
+            "password": "1234"
+        },
+        {
+            "name": "Andreas Huber",
+            "email": "a.huber@web.de",
+            "password": "4321"
+        }
+    ];
+
     await backend.setItem('users', JSON.stringify(users));
 }
+
+async function saveTasksToBackend() {
+    tasks = [
+        {
+            "title": "Webdesign redesign",
+            "description": "Modify the contents of the main website. Adjust the UI to the company's brand design.",
+            "category": "Sales",
+            "assignedTo": ['You', 'Maximilian Vogel', 'Fritz Meier'],
+            "dueDate": "01.01.2023",
+            "priority": "Medium",
+            "subTasks": ['Create new icons']
+        },
+        {
+            "title": "Price increase",
+            "description": "Price increase Description...",
+            "category": "Back Office",
+            "assignedTo": ['You'],
+            "dueDate": "01.02.2023",
+            "priority": "High",
+            "subTasks": ['Inform customers', 'Sales Manager Meeting']
+        }
+    ];
+
+    await backend.setItem('tasks', JSON.stringify(tasks));
+}
+
 
 function goToSignUpPage() {
     window.location.href = 'signup.html';
 }
 
 
-async function deleteAllUsers() {
+async function deleteAll() {
     await backend.deleteItem('users');
+    await backend.deleteItem('tasks');
 }
+
+
+
 
 /**
  * Show error message
