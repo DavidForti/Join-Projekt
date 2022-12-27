74a2b104257c2bf44bb71f4cc00ea3944fe65665
DavidForti <110557526+DavidForti@users.noreply.github.com>
2022-12-27 17:19:55 +0100

    updSumary

--- js/summary_js/summary.js
+++ js/summary_js/summary.js
@@ -1,21 +1,36 @@
-let taskInBoard = [5,4,5];
+let taskInBoard = [0];
 // let tasInProgress = [4];
 // let feedBack = [];
 
 function summary() {
    let contantSummary =  document.getElementById('contantSummary');
    contantSummary.innerHTML = '';
+   contantSummary.innerHTML = hedalineSummary();
     for (let i = 0; i < taskInBoard.length; i++) {
         const board = taskInBoard[i];
-        // const progress = tasInProgress[i];
-        // const feedBacks = feedBack[i];
-        contantSummary.innerHTML = /*html*/`
-            <div class=head-of-summary>
-                <h1 class="head-summary">Summary</h1>
-            </div>
-            
-        `;
+        contantSummary.innerHTML += progrsses();
 
     }
 }
 
+function hedalineSummary (){
+    return /*html*/`
+    <div class=head-of-summary>
+        <h1 class="head-summary">Summary</h1>
+        <img src="/img/verticalLine.png" class="vertical-Line">
+        <img src="/img/nutshell.png" class="nutshell">
+    </div>
+    
+`;
+}
+
+function progrsses(){
+    return/*html*/`
+     <div class="boxes">
+        <div class="box"></div>
+        <div class="box"></div>
+        <div class="box"></div>
+     </div>
+    
+    `;
+}
