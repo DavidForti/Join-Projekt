2e1ce42d7a39e2490014661cf31aeac61d7ab71a
Don Wagner <wagner.don@protonmail.com>
2022-12-30 09:52:57 -0300

    notice legal

--- js/legalNotice_js/legalNotice.js
+++ js/legalNotice_js/legalNotice.js
@@ -2,7 +2,41 @@ function legalNotice() {
     let content = document.getElementById('legal-notice-container');
     content = "";
     content += `
-        
+    <h1 class="h1n">
+    Legal Notice
+</h1>
+<h2 class="h2n">
+    Subtitle
+</h2>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam iure ratione repellat minima, reiciendis odit
+    quos modi recusandae nulla unde voluptatem tenetur mollitia voluptatibus enim, sapiente quis delectus tempore
+    voluptates.
+    Totam ut rerum officia inventore odit, debitis dolore, sed ipsum nam officiis similique blanditiis iste magnam
+    quo maxime aliquid, cumque molestias incidunt! Natus in dolorum cumque facere, placeat aliquam necessitatibus!
+    Obcaecati possimus illo magni cupiditate sapiente facere dicta doloremque, aliquam dolores architecto laudantium
+    sit perspiciatis quod corrupti, at iusto? Magnam ratione delectus ipsum quae eos praesentium sunt cupiditate
+    eaque voluptatem.
+    Eos iste placeat itaque quisquam molestias architecto ratione. Saepe, reprehenderit libero enim mollitia facere
+    itaque natus at ullam perferendis placeat distinctio, nam id vitae? Pariatur ullam neque ipsum id molestias.
+    Accusantium excepturi aliquam totam in inventore, alias et. Vitae minima magni harum sequi hic mollitia expedita
+    voluptatum illum culpa voluptatibus itaque laudantium a ipsum voluptate, similique labore cumque molestias
+    delectus?</p>
+<h2 class="h2n">
+    Subtitle
+</h2>
+<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam iure ratione repellat minima, reiciendis odit
+    quos modi recusandae nulla unde voluptatem tenetur mollitia voluptatibus enim, sapiente quis delectus tempore
+    voluptates.
+    Totam ut rerum officia inventore odit, debitis dolore, sed ipsum nam officiis similique blanditiis iste magnam
+    quo maxime aliquid, cumque molestias incidunt! Natus in dolorum cumque facere, placeat aliquam necessitatibus!
+    Obcaecati possimus illo magni cupiditate sapiente facere dicta doloremque, aliquam dolores architecto laudantium
+    sit perspiciatis quod corrupti, at iusto? Magnam ratione delectus ipsum quae eos praesentium sunt cupiditate
+    eaque voluptatem.
+    Eos iste placeat itaque quisquam molestias architecto ratione. Saepe, reprehenderit libero enim mollitia facere
+    itaque natus at ullam perferendis placeat distinctio, nam id vitae? Pariatur ullam neque ipsum id molestias.
+    Accusantium excepturi aliquam totam in inventore, alias et. Vitae minima magni harum sequi hic mollitia expedita
+    voluptatum illum culpa voluptatibus itaque laudantium a ipsum voluptate, similique labore cumque molestias
+    delectus?</p>
     `;
 
 }
